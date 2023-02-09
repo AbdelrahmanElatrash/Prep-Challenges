6e1d22b132a51b313c317fe4ad9a4f0ce57f4c11
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2023-02-09 15:10:30 +0000

    fix the first function to hold negative number

--- code-challenges/challenges-06.js
+++ code-challenges/challenges-06.js
@@ -14,7 +14,7 @@
 const findMax = (arr)=>{
     let max;
     // write your code here
-    max=0 ;
+    max=arr[0] ;
     for(let i=0;i<arr.length ; i++){
         if(arr[i]>max){
             max=arr[i];
